b2fd5a81870ba0accb16acd422e78cbb82eb5d91
senamatrac <senamatrac@gmail.com>
2023-08-11 18:16:08 +0300

    working...

---
+++ src/main/java/io/upschool/dto/response/BaseResponse.java
@@ -0,0 +1,25 @@
+package io.upschool.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class BaseResponse<T> {
+    private boolean isSuccess;
+    private int status;
+    @Builder.Default
+    @JsonProperty("Current Timestamp")
+    private LocalDateTime currentTimestamp = LocalDateTime.now();
+    private Map<String, Object> responseBody;
+    private T data;
+
+}
